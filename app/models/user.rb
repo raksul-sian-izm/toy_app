96473f62bfd59ac314a77f0850187d2c30983408
s.izumida <s.izumida@raksul.com>
2020-02-27 18:18:00 +0700

    introduce pagination

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  paginates_per 20
+
   has_many :microposts
   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :name, presence: true
