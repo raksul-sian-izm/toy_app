60d56b6f22d625b196cca6dbc3ffda5de24921d3
yun <dungnv005@gmail.com>
2020-03-02 15:14:11 +0700

    Add user avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   has_many :microposts
   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :name, presence: true
+  has_one_attached :avatar
 end
